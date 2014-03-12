081c6d192f314b867d8e8d6644ef318101a41ca2
kendomination <kennethcanlas630@gmail.com>
2014-03-13 01:02:39 +0800

    1st commit

---
+++ js/changeimg.js
@@ -0,0 +1,28 @@
+
+function change_explore()
+{
+	document.getElementById("start").src = "resources/exploreclicked.png";
+}
+function change_about()
+{
+	document.getElementById("about").src = "resources/aboutclicked.png";
+}
+function change_back()
+{
+	document.getElementById("back").src = "resources/arrow2.png";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
